e788bd3bb3c929df34a169cd15d78845ce1323e0
Erich F Schwarzrock <efschwarzrock@wpi.edu>
2019-09-21 14:01:54 -0400

    Create TimeCode.c

---
+++ C/TimeCode.c
@@ -0,0 +1,38 @@
+/* doit.c
+ * Erich Schwarzrock
+ * */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <time.h>
+#include <sys/time.h>
+
+#include <string.h>
+
+
+//Main function
+int main(int argc, char *argv[]){
+/* argc - num of arguments
+ * argv - an array of strings
+*/
+	
+	//worldclock start and end times
+	struct timeval wcStart;
+	struct timeval wcEnd;
+	
+		//start timer
+		gettimeofday(&wcStart, NULL);
+	  //code
+    
+    //code
+    //end timer
+	  gettimeofday(&wcEnd, NULL);
+		printf("World-clock time : %ld\n", ((wcEnd.tv_sec*1000000 
+			+ wcEnd.tv_usec) - (wcStart.tv_sec * 1000000 +
+			 wcStart.tv_usec)));	
+			
+		
+}
